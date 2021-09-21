85b48fbf2d1b0f032fde4897fd77ddcd2c9fb784
denne <denne@DESKTOP-OIHJ20M>
2021-09-21 15:09:18 +0800

    springWindow

--- SpringMVC20210906/src/main/java/com/mvc/controller/Hello.java
+++ SpringMVC20210906/src/main/java/com/mvc/controller/Hello.java
@@ -3,7 +3,6 @@ package com.mvc.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
